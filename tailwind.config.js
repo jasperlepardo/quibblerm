98af4a36608773b6faf6bf457cd08720c1b65904
Jasper Lepardo <jsprlprd@gmail.com>
2023-08-17 03:51:35 +0800

    sync

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html"],
   theme: {
-    extend: {},
+    container: {
+      screens: {
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+      },
+      maxWidth: '100%',
+      '@screen sm': {
+        maxWidth: '540px',
+      },
+      '@screen md': {
+        maxWidth: '720px',
+      },
+      '@screen lg': {
+        maxWidth: '960px',
+      },
+      '@screen xl': {
+        maxWidth: '1140px',
+      },
+    },
+    fontFamily: {
+      'quicksand': ['Quicksand', 'sans-serif'],
+      'commissioner': ['Commissioner', 'sans-serif'],
+    },
+    extend: {
+    },    
   },
   plugins: [],
 }
